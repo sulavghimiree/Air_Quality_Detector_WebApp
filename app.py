709b6a1883d894d193cbc3ca5c1f51eb52410e0c
Sulav Ghimire <sulavg2003@gmail.com>
2025-01-03 19:16:29 +0545

    made the simple form tab

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     features = [float(x) for x in request.form.values()]
+    print(features)
     final_features = [np.array(features)]
 
     prediction = model.predict(final_features)
@@ -27,7 +28,35 @@ def predict():
     else:
         output = 'Poor'
     
-    return render_template('index.html', prediction_text='Air Quality is {}'.format(output))
+    return render_template('result.html', prediction_text='Air Quality is {}'.format(output))
+
+@app.route('/predict_general', methods=['POST'])
+def predict_general():
+    if request.is_json:
+        data = request.get_json()
+        features = [data['temp'], data['humidity'], data['pm2'], data['pm10'], data['no2'], data['so2'], data['co'], data['proximity'], data['populationDensity']]
+        print(features)
+        final_features = [np.array(features)]
+
+        prediction = model.predict(final_features)
+        if prediction[0] == 0:
+            output = 'Good'
+        elif prediction[0] == 1:
+            output = 'Hazardious'
+        elif prediction[0] == 2:
+            output = 'Moderate'
+        else:
+            output = 'Poor'
+        
+        print(output)
+
+        try:
+            return render_template('result.html', prediction_text='Air Quality is {}'.format(output))
+        except:
+            print('error')
+    else:
+        print('error')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
